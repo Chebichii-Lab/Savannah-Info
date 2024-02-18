d37439af3c5b83468a2ac4932e49a0fa6ee48299
Chebichii-Lab <chebichiiserem@gmail.com>
2024-02-18 12:34:59 +0300

    alter the model class order and migrate the changes to the database

--- to-do/todo_api/models.py
+++ to-do/todo_api/models.py
@@ -10,9 +10,9 @@ class Customer(models.Model):
         return self.name
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
     item = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return f"{self.item} - {self.customer.name}"
+        return f"{self.item} - {self.customer.name if self.customer else 'Unknown'}"
